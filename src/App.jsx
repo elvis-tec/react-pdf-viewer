43f688e62af236b7b0becb78c128b9daaa74da2e
Elvis Salazar <e2.salazar.acuna.elvis@gmail.com>
2022-11-08 12:10:24 -0600

    PDF viewer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
+import { PdfViewer } from './components/PdfViewer';
+import samplePdf from './assets/sample.pdf';
 
 function App() {
   return (
     <div className="App">
-      
+      <PdfViewer pdf={samplePdf} />
     </div>
   )
 }
---
+++ src/components/PdfViewer.jsx
@@ -0,0 +1,40 @@
+import React, { useRef, useState } from 'react';
+import { Document, Page } from 'react-pdf/dist/esm/entry.vite';
+import { GrNext, GrPrevious } from 'react-icons/gr';
+
+export const PdfViewer = ({pdf}) => {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+    const divRef = useRef();
+
+    const onDocumentLoadSuccess = ({ numPages })=>{
+        setNumPages(numPages);
+    }
+    
+    const handleNextPage = ()=>{
+        if(pageNumber < numPages){
+            setPageNumber( prevState => prevState + 1 );
+        }
+    }
+    
+    const handlePreviousPage = ()=>{
+        if(pageNumber > 1){
+            setPageNumber( prevState => prevState - 1 );
+        }
+    }
+
+  return (
+    <div ref={divRef}>
+        <div>{pageNumber} / {numPages}</div>
+        <div style={{ margin: "10px 0"}}>
+            { pageNumber > 1 && <button onClick={handlePreviousPage}><GrPrevious /></button>}
+            { pageNumber < numPages && <button onClick={handleNextPage}><GrNext /></button>}
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} width={divRef.current?.clientWidth * 0.95} />
+            </Document>
+        </div>
+    </div>
+  )
+}
